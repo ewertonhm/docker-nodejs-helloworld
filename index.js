596bcb551dba4ec5028819462b0f81f69b6ea86d
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2023-09-11 17:52:40 -0300

    move log to a separated middleware

--- index.js
+++ index.js
@@ -4,8 +4,22 @@ var express = require('express');
 var DEFAULT_PORT = 8080;
 var PORT = process.env.PORT || DEFAULT_PORT;
 
+// logger middleware
+const logger = (options) =>
+(req, res, next) => { 
+  const timestamp = new Date().toISOString(); 
+    const { method, url, ip } = req; 
+      console.log(`
+          ${timestamp} 
+          ${options.level} 
+          ${method} ${url} 
+          ${ip}`); 
+        next(); 
+  };  
+
 // App
 var app = express();
+app.use(logger({ level: 'INFO' }));
 app.get('/', function (req, res) {
   console.log(req);
   res.send('Hello World\n');
